4b9ef6a2607943e8ab1e0dc38e95f6947912cb49
Bhagyesh Dhanashre <138644030+BhagyeshDhanashre@users.noreply.github.com>
2025-11-21 22:34:22 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        dbname="fastapi",
-        user="postgres",
-        password=83100,
+        dbname="DATABASE_NAME",
+        user="USER_NAME",
+        password=YOUR_PASSWORD,
         host="localhost",
         port="5432"
     )
